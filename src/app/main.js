89450a331bba20c239fccb013ddff7a56b52a8f3
Fan <fan.yu@philips.com>
2018-03-21 22:36:14 +0800

    update

--- src/app/main.js
+++ src/app/main.js
@@ -12,30 +12,7 @@ angular.module('app')
 
       // config
       $scope.app = {
-        name: 'Angulr',
-        version: '1.3.3',
-        // for chart colors
-        color: {
-          primary: '#7266ba',
-          info:    '#23b7e5',
-          success: '#27c24c',
-          warning: '#fad733',
-          danger:  '#f05050',
-          light:   '#e8eff0',
-          dark:    '#3a3f51',
-          black:   '#1c2b36'
-        },
-        settings: {
-          themeID: 1,
-          navbarHeaderColor: 'bg-black',
-          navbarCollapseColor: 'bg-white-only',
-          asideColor: 'bg-black',
-          headerFixed: true,
-          asideFixed: false,
-          asideFolded: false,
-          asideDock: false,
-          container: false
-        }
+       
       }
 
       // save settings to local storage
